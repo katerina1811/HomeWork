5ec52c636c321f247ad7b70831ce853f9dc92a39
ekaterina <katerinka.kuzmina.94@inbox.ru>
2022-07-11 18:52:22 +0300

    home_work9

---
+++ seminar_9/Program.cs
@@ -0,0 +1,77 @@
+﻿/* Задача 64: Задайте значения M и N. Напишите программу, которая выведет все
+натуральные числа в промежутке от M до N.
+M = 1; N = 5. -> "1, 2, 3, 4, 5"
+M = 4; N = 8. -> "4, 5, 6, 7, 8"
+M = 8; N = 4. -> "8, 7, 6, 5, 4" */
+
+/* int M = InputNumbers("Введите M: ");
+int N = InputNumbers("Введите N: ");
+PrintNumber(M, N);
+
+void PrintNumber(int M, int N)
+{
+    if (M != N) {
+        Console.Write(M + ", ");
+    } else {
+        Console.Write(M);
+        return;
+    }
+    
+    PrintNumber(M + 1, N);
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+} */
+
+/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+/* Console.WriteLine("Введите начальное число M:");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число M:");
+int numberN = int.Parse(Console.ReadLine());
+
+void GapNumberSum(int numberM, int numberN, int sum)
+{
+    if (numberM > numberN)
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}");
+        return;
+    }
+    sum = sum + (numberM++);
+    GapNumberSum(numberM, numberN, sum);
+}
+
+GapNumberSum(numberM, numberN, 0); */
+
+/* Задача 68: Напишите программу вычисления функции Аккермана с помощью
+рекурсии. Даны два неотрицательных числа m и n.
+m = 3, n = 2 -> A(m,n) = 29 */
+
+/* int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+
+int functionAkkerman = Ack(m, n);
+
+Console.Write($"Функция Аккермана = {functionAkkerman} ");
+
+int Ack(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return Ack(m - 1, 1);
+    else return Ack(m - 1, Ack(m, n - 1));
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+} */
